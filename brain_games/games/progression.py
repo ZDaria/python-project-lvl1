0aba9e81bc9a5cce479671d3d5b369f950890e31
Zavgorodneva Daria <d210927@yandex.ru>
2022-07-01 15:59:04 +0300

    Were added vars for ech number in ech expression.

--- brain_games/games/progression.py
+++ brain_games/games/progression.py
@@ -1,13 +1,28 @@
 import random
+"""This module creates task for progression game."""
+
+HIDDEN_START_NUM_POSITION_COUNT = 0
+
+LOW_LIMIT_PROGRESSION_LEN = 5
+UPPER_LIMIT_PROGRESSION_LEN = 11
+
+LOW_LIMIT_PROGRESSION_START = 1
+UPPER_LIMIT_PROGRESSION_START = 100
+
+LOW_LIMIT_STEP = 1
+UPPER_LIMIT_STEP = 10
 
 TASK_STRING = "What number is missing in the progression?"
 
 
 def get_task():
-    progression_start = random.randint(1, 100)
-    step = random.randint(2, 10)
-    progression_length = random.randint(5, 11)
-    hidden_num_position = random.randint(0, progression_length)
+    progression_start = random.randint(LOW_LIMIT_PROGRESSION_START,
+                                       UPPER_LIMIT_PROGRESSION_START)
+    step = random.randint(LOW_LIMIT_STEP, UPPER_LIMIT_STEP)
+    progression_length = random.randint(LOW_LIMIT_PROGRESSION_LEN,
+                                        UPPER_LIMIT_PROGRESSION_LEN)
+    hidden_num_position = random.randint(HIDDEN_START_NUM_POSITION_COUNT,
+                                         progression_length)
     hidden_num = 0
     progression_string = ""
     while progression_length >= 0:
